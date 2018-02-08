2e643da1a0a5e224a2b287aa36cfd87eb9f1daa6
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-08 12:07:28 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,8 @@ class School
     @roster={}
   end
 
-  def add_student([], student_grade)
-    @roster[student_grade] = [student_name]
+  def add_student(student_name[], student_grade)
+    @roster[student_grade] = student_name[]
   end
 
 end
